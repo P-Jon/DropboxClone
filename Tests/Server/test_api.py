651f46a5afa6cddfd44a0fceb8a9b9b7eb16f2f3
Jonathon Cross <j.cross3@lancaster.ac.uk>
2021-12-27 15:03:12 +0000

    Adding test templating, setting up for testing the business logic of the Server

--- Tests/Server/test_api.py
+++ Tests/Server/test_api.py
@@ -1,7 +1,15 @@
 import pytest
 
-def test_loud():
+from Server.Server import Server
+
+list_of_files = """
+{ "files" : [ { "filename" : "test.txt", "size" : "0", "last_edit" : "001"},
+{"filename" : "test2.txt", "size" : "2", "last_edit" : "002"} ] }"""
+
+test_server = Server()
+
+def test_get_metadata():
     assert "loud".upper() == "LOUD"
-    
+
 def test_lower():
     assert "LOWER".lower() == "lower"
---
+++ Tests/Server/test_server.py
@@ -0,0 +1,4 @@
+import pytest
+
+def test_val_dirs():
+    assert True
