458a8d6766462d1da881a900c0f2113f6e0d61cc
Jonathon Cross <j.cross3@lancaster.ac.uk>
2022-01-02 21:34:24 +0000

    Allowing to specify client or server debug mode

--- postbox/debugging/Logger.py
+++ postbox/debugging/Logger.py
@@ -1,11 +1,16 @@
 from postbox.helper.YAMLHandler import YAMLHandler
 class Logger():
-    def __init__(self):
+    def __init__(self, type):
+        '''
+        Variable 'Type' is whether the logger is handling client or server debugging
+        and adjusts to using the relevant configuration.
+        '''
+        self.config_type = "server"
         self.debug_mode = False
         self.get_config()
     
     def get_config(self):
-        dbm = YAMLHandler().get_variable("debug_mode") # Warning: can be set to None.
+        dbm = YAMLHandler().get_variable(self.config_type + "_debug_mode") # Warning: can be set to None.
         if (dbm != None):
             self.debug_mode = dbm
 
